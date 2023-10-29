c6837104df40e03b42caae11bb73b6ef9ac97046
alanjameschapman <>
2023-10-29 15:33:22 +0000

    generate 5 random numbers

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# 80 characters wide and 24 rows high max
+
+import random
+
+def roll_one():
+    """
+    Generates a list of five random integers between 1 and 6 to represent die face values
+    """
+    result = []
+    for die in range(5):
+        roll = random.randint(1, 6)
+        result.append(roll)
+    return result
+
+result = roll_one()
+print(result)
