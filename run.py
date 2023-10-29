5d11d58060cb3a41db5debb5b7891838cac6c72d
alanjameschapman <>
2023-10-29 22:46:43 +0000

    takes user input for keep choice, validates, and re-rolls remainder

--- run.py
+++ run.py
@@ -15,6 +15,37 @@ def roll_one():
     return result_one
 
 
+def user_choice(dice):
+    """Takes dice arg from previous roll and prompts the user to input which
+    dice should be kept. Returns a list of integers to retain.
+    """
+
+    # Print the current roll.
+    print(f'Your current roll is: {dice}')
+
+    # Prompt the user to input which dice to keep.
+    while True:
+        try:
+            dice_to_keep = input(
+                'Which dice do you want to keep? (Enter the '
+                'indices, separated by spaces): ')
+
+            # Split the input string into a list of integers.
+            dice_to_keep = [int(i) for i in dice_to_keep.split()]
+
+            # Validate the input.
+            for i in dice_to_keep:
+                if i < 0 or i >= len(dice):
+                    raise ValueError('Invalid index.')
+
+            # Break out of the loop if the input is valid.
+            break
+        except ValueError:
+            print('Invalid input. Please try again.')
+
+    return dice_to_keep
+
+
 def keep_and_reroll(dice, keep):
     """
     Takes dice arg from previous roll and re-rolls dice which aren't in keep
@@ -29,14 +60,11 @@ def keep_and_reroll(dice, keep):
     for die in range(len(dice)):
         if die not in keep:
             new_dice[die] = random.randint(1, 6)
-       
+
     return new_dice
 
 
 dice = roll_one()
-print(dice)
-
-# Keeps the first two dice and re-roll the rest.
-keep = [0, 1, 2]
+keep = user_choice(dice)
 new_dice = keep_and_reroll(dice, keep)
-print(new_dice)
+print(f'Your updated roll is {new_dice}. You have one roll remaining.')
