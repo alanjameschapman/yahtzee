cd680bc16e4f0df40464fd0184b86546d203b63d
alanjameschapman <>
2023-10-30 22:22:55 +0000

    create prompts and associated placeholders for leaderboard, rules, play, reroll, submit and exit.

--- run.py
+++ run.py
@@ -3,6 +3,39 @@
 import random
 
 
+def home():
+    """
+    Displays home screen and user prompt for leaderboard, rules or game.
+    """
+    print('ASCII art goes here')
+
+    while True:
+        try:
+            home_choice = input(
+                "Type 'l' for leaderboard, 'r' for rules, or 'p' to play.\n"
+                "Your choice: ")
+            if home_choice not in ['l', 'r', 'p']:
+                raise ValueError('Invalid choice')
+            break
+        except ValueError:
+            print('Invalid input. Please try again.')
+    return home_choice
+
+
+def leaderboard():
+    """
+    Generates a leaderboard from Google Sheet.
+    """
+    print('Leaderboard (top 10) to go here')
+
+
+def rules():
+    """
+    Displays rules. Clear screen method used to move to next page.
+    """
+    print('Rules to go here...')
+
+
 def roll_one():
     """
     Generates a list of five random integers between 1 and 6 to represent die
@@ -15,14 +48,29 @@ def roll_one():
     return result_one
 
 
-def user_choice(dice):
+def user_prompt(dice):
+    """
+    Prints the current roll and prompts user to re-roll, submit or exit.
+    """
+    while True:
+        try:
+            game_choice = input(
+                f'After your first roll, your dice are: {dice}\n'
+                "Type 'r' to re-roll some or all dice, 's' to submit score to"
+                " scoreboard, or 'e' to exit home.\n"
+                "Your choice: ")
+            if game_choice not in ['r', 's', 'e']:
+                raise ValueError('Invalid choice')
+            break
+        except ValueError:
+            print('Invalid input. Please try again.')
+    return game_choice
+
+
+def keep_choice(dice):
     """Takes dice arg from previous roll and prompts the user to input which
     dice should be kept. Returns a list of integers to retain.
     """
-
-    # Print the current roll.
-    print(f'Your current roll is: {dice}')
-
     # Prompt the user to input which dice to keep.
     while True:
         try:
@@ -64,7 +112,31 @@ def keep_and_reroll(dice, keep):
     return new_dice
 
 
+def submit(dice):
+    """
+    Evaluates score and adds to scoreboard once user selects box.
+    """
+    print('Scoreboard updates...')
+
+
+# Main code block
+home_choice = home()
+if home_choice == 'l':
+    leaderboard()
+elif home_choice == 'r':
+    rules()
+else:
+    name = input('Please enter your name: ')
+    print(f"{name}, let's play Yahtzee!\n")
+
 dice = roll_one()
-keep = user_choice(dice)
-new_dice = keep_and_reroll(dice, keep)
-print(f'Your updated roll is {new_dice}. You have one roll remaining.')
+game_choice = user_prompt(dice)
+
+if game_choice == 'e':
+    home()
+elif game_choice == 's':
+    submit(dice)
+else:
+    keep = keep_choice(dice)
+    new_dice = keep_and_reroll(dice, keep)
+    print(f'Your updated roll is {new_dice}. You have one roll remaining.')
